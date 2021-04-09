f46a79623b4d40e58621e4b81be7cb4e57538f67
Javier Chávarri <javier.chavarri@gmail.com>
2021-04-09 19:55:15 +0200

    remove unused widget and pages.json

--- plugins/add-code-tab.lua
+++ plugins/add-code-tab.lua
@@ -3,9 +3,9 @@ code_snippets = HTML.select(page, "code.language-ocaml")
 data = config
 tab_tmpl =
   "<div class=\"tab\">" ..
-  "<button class=\"button-language-ocaml tablinks\" onclick=\"openLangTab(event, 'language-ocaml')\">ml</button>" ..
-  "<button class=\"tablinks\" onclick=\"openLangTab(event, 'language-reason')\">re</button>" ..
-  "<button class=\"tablinks\" onclick=\"openLangTab(event, 'output-language-javascript')\">output (js)</button>" ..
+  "<button class=\"fs-3 btn button-language-ocaml tablinks\" onclick=\"openLangTab(event, 'language-ocaml')\">ml</button>" ..
+  "<button class=\"fs-3 btn tablinks\" onclick=\"openLangTab(event, 'language-reason')\">re</button>" ..
+  "<button class=\"fs-3 btn tablinks\" onclick=\"openLangTab(event, 'output-language-javascript')\">output (js)</button>" ..
   "</div>"
 
 index = 1
--- plugins/pages-index.lua
+++ plugins/pages-index.lua
@@ -5,8 +5,10 @@ container = HTML.select_one(page, "#pages-index")
 local n = 1
 local count = size(pages)
 while (n <= count) do
-  li = HTML.parse(String.render_template("<li><a href=\"/{{id}}/\">{{title}}</a></li>", pages[n]))
-  HTML.append_child(container, li)
+  if not pages[n].nav_path[1] then
+    li = HTML.parse(String.render_template("<li><a href=\"/{{url}}/\">{{title}}</a></li>", pages[n]))
+    HTML.append_child(container, li)
+  end
 
   n = n + 1
 end
