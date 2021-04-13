4d04e4165e68d7b398cdcd9559e728ba9d7b4ab8
Javier Chávarri <javier.chavarri@gmail.com>
2021-04-13 21:41:52 +0200

    fix syntax highlight for js and re

--- preprocessors/prism.js
+++ preprocessors/prism.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const Prism = require('prismjs');
 require('prismjs/components/prism-ocaml');
+require('prismjs/components/prism-reason');
+require('prismjs/components/prism-javascript');
 
 async function read(stream) {
   const chunks = [];
@@ -8,7 +10,22 @@ async function read(stream) {
   return Buffer.concat(chunks).toString('utf8');
 }
 
+let lang = process.argv.slice(2)[0];
+
+let syntax = Prism.languages.ocaml;
+switch (lang) {
+  case 'reason':
+    syntax = Prism.languages.reason;
+    break;
+  case 'javascript':
+    syntax = Prism.languages.javascript;
+    break;
+  default:
+    lang = 'ocaml';
+    syntax = Prism.languages.ocaml;
+}
+
 (async () => {
   const code = await read(process.stdin);
-  process.stdout.write(Prism.highlight(code, Prism.languages.ocaml, 'ocaml'));
+  process.stdout.write(Prism.highlight(code, syntax, lang));
 })();
